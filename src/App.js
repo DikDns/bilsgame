a39e2b9f9eac198b755bf2ee0fdb49918f525c6b
DikDns <dikdnssocial@gmail.com>
2022-06-02 18:03:36 +0700

    rmeove fontawesome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  solid,
-  regular,
-  brands,
-} from "@fortawesome/fontawesome-svg-core/import.macro";
 import "./css/main.css";
 
 function App() {
   return (
     <main>
       <h1>Hello World</h1>
-      <FontAwesomeIcon icon={brands("twitter")}></FontAwesomeIcon>
     </main>
   );
 }
