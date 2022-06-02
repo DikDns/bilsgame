38f57c48175f3e4664371b6d2b2a9524f9c9f4ef
DikDns <dikdnssocial@gmail.com>
2022-06-02 17:45:50 +0700

    add babel plugin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import "./";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  solid,
+  regular,
+  brands,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
+import "./css/main.css";
 
 function App() {
   return (
     <main>
       <h1>Hello World</h1>
+      <FontAwesomeIcon icon={brands("twitter")}></FontAwesomeIcon>
     </main>
   );
 }
