dc379d4692f066e1573d142b007f4e2c25b118f4
DikDns <dikdnssocial@gmail.com>
2022-06-03 14:57:18 +0700

    add onAnimationEnd event

--- src/components/DisplayBils.js
+++ src/components/DisplayBils.js
@@ -4,8 +4,11 @@ function DisplayBils(props) {
   return (
     <div
       id={props.index}
-      className={`bils my-2 ${props.selected ? `bg-info text-white` : ``}`}
-      onClick={(event) => props.onClick(event, props.index)}
+      className={`bils my-2 ${
+        props.selected ? `bg-info text-white shake` : ``
+      }`}
+      onClick={(event) => props.onClick(event, props)}
+      onAnimationEnd={(event) => props.onAnimationEnd(event, props)}
     >
       <span
         className={`h-100 fw-bold  d-flex justify-content-center align-items-center`}
