19bde93744b348ccf2d3cf8ad3e94d0390945cd7
DikDns <dikdnssocial@gmail.com>
2022-06-03 13:41:43 +0700

    add DisplayBils

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import DisplayBils from "./components/DisplayBils";
 import "./css/main.css";
 
 function App() {
   return (
     <main>
       <h1>Hello World</h1>
+      <DisplayBils></DisplayBils>
     </main>
   );
 }
---
+++ src/components/DisplayBils.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DisplayBils(props) {
+  return (
+    <div className="bils">
+      <span className="bils-number">0</span>
+    </div>
+  );
+}
+
+export default DisplayBils;
