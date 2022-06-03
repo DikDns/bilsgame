8b56ce3a24b15aa5b6d352f4cdf9785737faac04
DikDns <dikdnssocial@gmail.com>
2022-06-03 18:06:18 +0700

    add Audio components and rename sfx

---
+++ src/components/Audio.js
@@ -0,0 +1,31 @@
+import React from "react";
+// Import Music
+// import "../assets/music/main.mp3";
+// import "../assets/music/play.mp3";
+// import "../assets/music/sfx/bils-activated.wav";
+// import "../assets/music/sfx/bils-clicked.wav";
+// import "../assets/music/sfx/bils-duplicated.wav";
+// import "../assets/music/sfx/btn-clicked.wav";
+// import "../assets/music/sfx/count.wav";
+// import "../assets/music/sfx/lose.wav";
+// import "../assets/music/sfx/win.wav";
+
+function Audio(url) {
+  const [audio] = React.useState(new Audio(url));
+  const [playing, setPlaying] = React.useState(false);
+
+  React.useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  React.useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing];
+}
+
+export default Audio;
