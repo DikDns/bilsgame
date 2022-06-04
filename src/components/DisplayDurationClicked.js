c091126bc869fab8c222590e3cdeacf13eed9784
DikDns <dikdnssocial@gmail.com>
2022-06-04 12:44:48 +0700

    add style

--- src/components/DisplayDurationClicked.js
+++ src/components/DisplayDurationClicked.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function DisplayDurationClicked(props) {
   return (
-    <div className="clicked-duration">
+    <div className="clicked-duration w-100 h-25 bg-dark">
       <span className="duration-bar" id="duration-bar"></span>
     </div>
   );
