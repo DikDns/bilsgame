ee3785d5a361f8e34e7f5ee30023ac196594352b
DikDns <dikdnssocial@gmail.com>
2022-06-04 18:41:01 +0700

    add event handler

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,21 +2,46 @@ import React from "react";
 import "../css/main.css";
 
 const Game = () => {
+  const handlePlay = (e) => {
+    e.preventDefault();
+    console.log("Play");
+  };
+  const handleSettings = (e) => {
+    e.preventDefault();
+    console.log("Settings");
+  };
+  const handleCredits = (e) => {
+    e.preventDefault();
+    console.log("Credits");
+  };
+
   return (
     <main>
       <div className="main-menu py-5">
         <div className="play row mb-4 justify-content-center">
-          <button type="button" className="menuBtn menuBtn-primary">
+          <button
+            type="button"
+            className="menuBtn menuBtn-primary"
+            onClick={handlePlay}
+          >
             Play
           </button>
         </div>
         <div className="settings row mb-3 justify-content-center">
-          <button type="button" className="menuBtn menuBtn-secondary">
+          <button
+            type="button"
+            className="menuBtn menuBtn-secondary"
+            onClick={handleSettings}
+          >
             Settings
           </button>
         </div>
         <div className="credits row mb-3 justify-content-center">
-          <button type="button" className="menuBtn menuBtn-secondary">
+          <button
+            type="button"
+            className="menuBtn menuBtn-secondary"
+            onClick={handleCredits}
+          >
             Credits
           </button>
         </div>
