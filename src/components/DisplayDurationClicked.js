5881b780711de80b11ac1ec6a60c7901b9a936d7
DikDns <dikdnssocial@gmail.com>
2022-06-04 11:12:43 +0700

    add duration cliked component

---
+++ src/components/DisplayDurationClicked.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DisplayDurationClicked(props) {
+  return (
+    <div className="duration-clicked">
+      <span className="duration-indicator"></span>
+    </div>
+  );
+}
+
+export default DisplayDurationClicked;
