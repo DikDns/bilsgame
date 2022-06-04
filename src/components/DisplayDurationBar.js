59eadc0d7f602b64e929ac4107a2c5987ff6a83d
DikDns <dikdnssocial@gmail.com>
2022-06-04 12:46:18 +0700

    change name

---
+++ src/components/DisplayDurationBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DisplayDurationClicked(props) {
+  return (
+    <div className="clicked-duration w-100 h-25 bg-dark">
+      <span className="duration-bar" id="duration-bar"></span>
+    </div>
+  );
+}
+
+export default DisplayDurationClicked;
