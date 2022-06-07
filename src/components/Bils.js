cc0e9f8e01a744b96d3e21809d42450c62cde85f
DikDns <dikdnssocial@gmail.com>
2022-06-07 14:33:03 +0700

    add game_obj and game_settings

--- src/components/Bils.js
+++ src/components/Bils.js
@@ -1,84 +1,40 @@
-import React from "react";
-import bilsClickedSfx from "../assets/music/sfx/bils-clicked.wav";
-import bilsActivatedSfx from "../assets/music/sfx/bils-activated.wav";
-import bilsDuplicatedSfx from "../assets/music/sfx/bils-duplicated.wav";
-
-class Bils {
-  constructor(id, name, selected, sound) {
-    this.id = id;
-    this.name = name;
-    this.selected = selected;
-    this.doubled = false;
-    this.sound = {
-      activated: sound.activated,
-      clicked: sound.clicked,
-      duplicated: sound.duplicated,
-    };
+class BilsObj {
+  constructor(count = [], names = 1) {
+    if (names !== undefined && names.length < 1) {
+      this.names = names;
+    } else {
+      this.names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }
+    if (count >= 1) {
+      this.count = count;
+      this.bils = this.randomizeBils(count);
+    } else {
+      this.count = 5;
+      this.bils = this.randomizeBils(count);
+    }
   }
 
-  setDoubled(newDoubled) {
-    this.doubled = newDoubled;
-  }
-
-  setName(newName) {
-    this.name = newName;
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  setSelected(newSelected) {
-    this.selected = newSelected;
-  }
-
-  getSelected() {
-    return this.selected;
-  }
-
-  setSound(newSound) {
-    this.sound = newSound;
-  }
-
-  getSound() {
-    return this.sound;
+  randomizeBils(count, id = null) {
+    if (id === null && count >= 1) {
+      const arrBils = [];
+      const maxName = this.names.length - 1;
+      for (let i = 0; i < count; i++) {
+        let randomNum = Math.round(Math.random() * maxName);
+        randomNum = randomNum === maxName ? --randomNum : randomNum;
+        arrBils.push(new Bil(i, randomNum));
+      }
+      return arrBils;
+    }
   }
 }
 
-const bilsSound = {
-  clicked: new Audio(bilsClickedSfx),
-  activated: new Audio(bilsActivatedSfx),
-  duplicated: new Audio(bilsDuplicatedSfx),
-};
-
-const spawnBils = (count) => {
-  const bils = [];
-
-  for (let i = 0; i < count; i++) {
-    let random = Math.round(Math.random() * 10);
-    random = random === 10 ? --random : random;
-    bils.push(new Bils(i, random, false, bilsSound));
+class Bil {
+  constructor(id, name, selected = false, duplicated = false) {
+    this.id = id;
+    this.name = name;
+    this.selected = selected;
+    this.duplicated = duplicated;
   }
-  return bils;
-};
+}
 
-const DisplayBils = ({ gameObject, handleClick, handleAnimationEnd }) => {
-  return (
-    <div
-      id={gameObject.id}
-      className={`bils my-2 ${
-        gameObject.selected ? `shake bg-info text-white` : `shake`
-      }`}
-      onClick={(event) => handleClick(event, gameObject)}
-      onAnimationEnd={(event) => handleAnimationEnd(event, gameObject)}
-    >
-      <span
-        className={`h-100 fw-bold d-flex justify-content-center align-items-center`}
-      >
-        {gameObject.name}
-      </span>
-    </div>
-  );
-};
-
-export { Bils, spawnBils, DisplayBils };
+export { BilsObj, Bil };
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,50 @@
 import React from "react";
 import "../css/main.css";
 
+import { BilsObj } from "./Bils.js";
+
+class GameSettings {
+  constructor() {
+    this.playState = false;
+  }
+
+  setPlayState(play = true) {
+    this.playState = play;
+  }
+
+  getPlayState() {
+    return this.playState;
+  }
+}
+
+const GAME_SETTINGS = new GameSettings();
+const GAME_OBJECT = { bilsObj: undefined };
+
 const Game = () => {
+  const [game, setGame] = React.useState({
+    gameObj: GAME_OBJECT,
+    settings: GAME_SETTINGS,
+  });
+
   const handlePlay = (event) => {
     event.preventDefault();
-    console.log("hello play");
+
+    GAME_SETTINGS.setPlayState(!GAME_SETTINGS.playState);
+    setGame((prevGame) => ({ settings: GAME_SETTINGS, ...prevGame }));
+
+    if (game.settings.playState) {
+      console.log("started");
+      GAME_OBJECT.bilsObj = new BilsObj(3);
+      setGame((prevGame) => ({ gameObj: GAME_OBJECT, ...prevGame }));
+    } else {
+      GAME_OBJECT.bilsObj = undefined;
+      setGame((prevGame) => ({ gameObj: GAME_OBJECT, ...prevGame }));
+    }
   };
+
+  React.useEffect(() => {
+    console.log(game);
+  }, [game]);
 
   return (
     <main>
@@ -17,7 +56,7 @@ const Game = () => {
             className="menuBtn menuBtn-primary"
             onClick={handlePlay}
           >
-            Play
+            {game.settings.playState ? "Stop" : "Start"}
           </button>
         </div>
       </div>
